0de301510415f91997d5b89d603ec7428b8ab7c4
Nafees <nafees56752@gmail.com>
2025-06-16 17:16:40 +0530

    quick links updated

--- src/helpers/footer.tsx
+++ src/helpers/footer.tsx
@@ -75,8 +75,9 @@ export const footerSocilalMedia = [
 export const footerLinks = [
     { name: 'Home', path: '/' },
     { name: 'Annual Calendar', path: '/home/annual-calendar' },
-    { name: 'Appointment', path: '/' },
-    { name: 'Events', path: '/' },
-    { name: 'About Us', path: '/' },
-    { name: 'Gallery', path: '/' }
+    { name: 'Appointment', path: '/home/book-appointment' },
+    { name: 'Find A Doctor', path: '/home/doctors' },
+    { name: 'Contact', path: '/home/contact' },
+    { name: 'Events', path: '/home/event' },
+    { name: 'About Us', path: '/home/about' },
 ]
